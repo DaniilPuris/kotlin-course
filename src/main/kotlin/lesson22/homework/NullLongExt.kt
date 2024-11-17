11ff9aa32f981985a1e098445a50f3d79faa30f0
Daniil Puris <atlasshrugged2033@gmail.com>
2024-11-17 21:01:56 +0500

    Files for Lesson22

--- src/main/kotlin/lesson22/homework/NullLongExt.kt
+++ src/main/kotlin/lesson22/homework/NullLongExt.kt
@@ -1,2 +1,12 @@
 package com.daniilpuris.lesson22.homework
 
+/* Функция-расширение для класса длинного числа, допускающее null,
+   которая не принимает аргументов и возвращает строку.
+ */
+
+fun Long?.toDescription(): String {
+    return when {
+        this == null -> "Null"
+        else -> "Not null"
+    }
+}
